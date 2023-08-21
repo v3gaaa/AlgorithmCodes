c595f5a778849fe7ec4332000113ba7f6110b71e
Sebastian Denhi Vega Saint Martin <104808082+v3gaaa@users.noreply.github.com>
2023-08-21 05:40:57 +0000

    Entrega

--- SumaN-1.cpp
+++ SumaN-1.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 
 // Función para calcular la sumatoria de 1 hasta n de forma iterativa
+// La complejidad de tiempo de este algoritmo es O(n) ya que se ejecuta solamente un for n veces
 int sumaIterativa(int n,int suma) {
     for (int i = 1; i <= n; i++) {
         suma += i;
@@ -15,6 +16,7 @@ int sumaIterativa(int n,int suma) {
 }
 
 // Función para calcular la sumatoria de 1 hasta n de forma recursiva
+//La complejidad de tiempo de este algoritmo es O(n) ya que es una función recursiva que se llama n veces
 int sumaRecursiva(int n, int suma) {
     if (n == 0) {
         return suma;
@@ -25,6 +27,8 @@ int sumaRecursiva(int n, int suma) {
 }
 
 // Función para calcular la sumatoria de 1 hasta n de forma directa
+//La complejidad de tiempo de este algoritmo es O(1) ya que solo se hace una operación con tiempo constante
+
 int sumaDirecta(int n) {
     return (n * (n + 1)) / 2;
 }
@@ -36,7 +40,12 @@ int main() {
 
     cout << "Ingrese un numero n: ";
     cin >> n;
-    
+
+    //Verificar que el numero sea positivo
+    while (n < 0) {
+        cout << "Ingrese un numero n positivo: ";
+        cin >> n;
+    }
     
     // Calcular la sumatoria de 1 hasta n utilizando las tres funciones
     int resultIterativo = sumaIterativa(n,suma);  
